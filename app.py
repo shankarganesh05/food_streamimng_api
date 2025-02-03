a8ad205055ae15a1a8ce78acfc5723dedefc2496
shankarganesh05 <shankarganesh.d@gmail.com>
2025-02-03 21:06:42 +0530

    Intialised with UV

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Query
 import pandas as pd
-import boto3
+# import boto3
 from io import StringIO
 import uvicorn
 
@@ -10,7 +10,7 @@ app = FastAPI()
 def fetch_data(year: int = None, country: str = None, market: str = None):
     try:
         # Load CSV content into a pandas DataFrame
-        df = pd.read_csv("https://food-data-test.s3.us-east-2.amazonaws.com/total_data.csv")
+        df = pd.read_csv("https://food-test-data05.s3.us-east-1.amazonaws.com/total_data.csv")
 
         print(df.shape[0])
         # Apply filters based on provided parameters
