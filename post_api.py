7666c21d177677b0fbc87db6d99c6e99c6c82653
shankarganesh05 <shankarganesh.d@gmail.com>
2025-02-16 08:12:23 +0530

    Modified the Post API file

--- post_api.py
+++ post_api.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI, Request
-import json
+from pydantic import BaseModel
 import uvicorn
 
 app=FastAPI()
 
+class Item(BaseModel):
+    name: str
+    Age: int
+
 @app.post('/post_data')
-def post_data():
-    message = Request.json
-    return message
+async   def post_data(item: Item):
+    print(item)
+    return item
 
 if __name__ == '__main__':
     uvicorn.run(app, port=5000, host='0.0.0.0')
